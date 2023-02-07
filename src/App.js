ada3226a282bcf6134947d764c8b27cadf96b139
Julien Edwards <60909020+TrunkOfUkuleles@users.noreply.github.com>
2023-02-07 10:23:15 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ if (popped==="none"){
       {/* <div id="Header-Stuffs">Contact Us</div> */}
     </div>
       <div id="Main-Body">
-        <div id="popout" visible="hidden" width="500px;" height="500px">{shoepic}</div>
+        <div id="popout" visible="hidden" width="500px;" height="500px">{holder}</div>
         <div id="section1" className="section-container">
           <div id="section1-title" className="section-title"><h2>Preservation Vs Time and Environment</h2></div>
           <div id="section1-cont">Keeping your goods fresh has always been a struggle. From everyday foods to the historical and cultural artifacts we use to look 
